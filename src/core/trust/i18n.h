31475d2ea7769aba49d26e061b01334ba3161219
thomas-voss <thomas.voss@canonical.com>
2014-09-19 17:18:15 +0200

    Export symbols. Install translations.

--- src/core/trust/i18n.h
+++ src/core/trust/i18n.h
@@ -19,6 +19,8 @@
 #ifndef CORE_TRUST_I18N_H_
 #define CORE_TRUST_I18N_H_
 
+#include <core/trust/visibility.h>
+
 #include <string>
 
 namespace core
@@ -28,15 +30,15 @@ namespace trust
 namespace i18n
 {
 // Returns the default text domain of this package.
-std::string default_text_domain();
+CORE_TRUST_DLL_PUBLIC std::string default_text_domain();
 // Returns the text domain of the service we are acting for
-std::string service_text_domain();
+CORE_TRUST_DLL_PUBLIC std::string service_text_domain();
 // Adjusts the text domain of the service;
-void set_service_text_domain(const std::string& domain);
+CORE_TRUST_DLL_PUBLIC void set_service_text_domain(const std::string& domain);
 // Translates the given input string for the default domain.
-std::string tr(const std::string& in);
+CORE_TRUST_DLL_PUBLIC std::string tr(const std::string& in);
 // Translates the given input string for the given domain.
-std::string tr(const std::string& in, const std::string& domain);
+CORE_TRUST_DLL_PUBLIC std::string tr(const std::string& in, const std::string& domain);
 }
 }
 }
