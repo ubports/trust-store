90ce94e02080c02cbf2e070eb3684b7efb9caf84
Alberto Aguirre <alberto.aguirre@canonical.com>
2014-10-03 17:09:14 -0500

    Attempt mir connection more than once

--- include/core/trust/mir_agent.h
+++ include/core/trust/mir_agent.h
@@ -22,6 +22,7 @@
 #include <core/trust/visibility.h>
 
 #include <memory>
+#include <stdexcept>
 
 // Forward declare the MirConnection type.
 struct MirConnection;
@@ -35,6 +36,15 @@ class Agent;
 
 namespace mir
 {
+
+class InvalidMirConnection : public std::runtime_error
+{
+public:
+    explicit InvalidMirConnection(const char* what_arg)
+        : std::runtime_error(what_arg)
+    {}
+};
+
 /**
  * @brief Helper function building up a connection to Mir.
  * @param endpoint The name of the endpoint exposed by the Mir instance.
@@ -46,7 +56,7 @@ CORE_TRUST_DLL_PUBLIC MirConnection* connect(const std::string& endpoint, const 
 /**
  * @brief create_agent_for_mir_connection creates a trust::Agent implementation leveraging Mir's trusted prompting API.
  * @param connection An existing connection to a Mir instance.
- * @throws std::logic_error if the connection object is NULL.
+ * @throws InvalidMirConnection if the connection object is invalid.
  */
 CORE_TRUST_DLL_PUBLIC std::shared_ptr<core::trust::Agent> create_agent_for_mir_connection(MirConnection* connection);
 }
